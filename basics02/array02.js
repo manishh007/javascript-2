55cf915ff719f1397cac7df0d6bf3c31ead10135
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2024-09-30 21:27:05 +0000

    more function of array

---
+++ basics02/array02.js
@@ -0,0 +1,40 @@
+let myArr = [1,2,3,4]
+// console.log(myArr);
+
+
+const marvelHeros = ["thor", "hulk", "ironman"]
+const dc = ["batman", "flash", "superman"]
+
+// marvelHeros.push(dc)
+// console.log(marvelHeros); //this takes whole array as a object and push it into the array. also we can acces the elements of an array by their index
+
+// const allHeros = marvelHeros.concat(dc)
+// console.log(marvelHeros);
+// console.log(dc);
+// console.log(allHeros);
+
+// const allHerosNew = [...marvelHeros,...dc]  //it also concat arrays like concat function
+// console.log(allHerosNew);  
+
+// const a1 = [1,2,[3,4],5,[6,7,[8,9]]]
+// const a2 = a1.flat(Infinity)   //it opens all the nested arrays and give a new individual array.
+// console.log(a2); 
+
+// console.log(Array.isArray("Manish"));
+// console.log(Array.from("Manish"));
+// console.log(Array.from({name: "Manish"}));   //it can't directly convert it into array so gives an empty array.
+
+// let a = 20
+// let b = 30
+// let c = 40
+// console.log(Array.of(a,b,c));
+
+//isArray : it is used to check whether the given object is array or not 
+//from : it is used to make array from the given data(string)
+//of : it is used to make array of many data 
+
+
+
+
+
+
