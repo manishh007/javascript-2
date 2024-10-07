e89796882e6c11edd8aa39a03ab68c6e14efa547
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2024-10-07 20:32:40 +0000

    more about object in js

--- basics02/objects03.js
+++ basics02/objects03.js
@@ -1,4 +1,4 @@
-// objects 
+/* // objects 
 
 
 //objects literals
@@ -12,9 +12,9 @@ const user1 = {
 
 }
 
-/* console.log(user1.name);
-console.log(user1["location"]);
-console.log(user1["year"]); */
+//  console.log(user1.name);
+// console.log(user1["location"]);
+// console.log(user1["year"]); 
 
 // how to declare a symbol
 
@@ -29,7 +29,7 @@ const user2 = {
 
 // console.log(user2[sym]);
 
-/* we can simply update any values by just overwrite it by using = sign */
+//  we can simply update any values by just overwrite it by using = sign 
 
 user1.location = "bulandshahr"
 // console.log(user1); 
@@ -54,3 +54,63 @@ user2.namaste = function(){
     
 }
 console.log(user2.namaste());
+
+
+ */
+
+// ****************************************more about objects*********************************
+
+const instaUser = new Object()  //singleton object
+// console.log(instaUser);
+
+const instaUser2 = {
+    email: "abc@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "manish",
+            lastname: "lodhi"
+
+        }
+    }
+}
+// console.log(instaUser2);    //non-singleton object
+
+instaUser.id = "1122"
+instaUser.name = "mann"
+instaUser.isLoggedIn = true
+
+// console.log(instaUser);
+// console.log(instaUser2);
+// console.log(instaUser2.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = {obj1,obj2} 
+const obj4 = Object.assign({}, obj1,obj2)  //here we write {} so to put all the assigned result to the empty object
+// console.log(obj3);
+// console.log(obj4);
+
+//best method to merge object/
+const obj5 = {...obj1, ...obj2}    //spread and conbine 
+// console.log(obj5);
+
+// console.log(Object.keys(instaUser));   //print only keys 
+// console.log(Object.values(instaUser));   //print only values
+// console.log(Object.entries(instaUser));   //put keys and values in a array inside a array
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
