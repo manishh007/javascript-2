8d6436e63c459ed69f650ad28c9ec82ce5fe58a4
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2024-10-08 21:18:17 +0000

    basics of functions in js

---
+++ basics03/functions01.js
@@ -0,0 +1,53 @@
+// function count(){
+//     console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// } 
+
+// count()
+
+// function addition(num1,num2){
+//      console.log(num1 + num2);
+     
+// }
+// const res = addition(5, 6)
+// console.log(res); // it shows undefined because console is a method to print it does not return anything
+
+
+
+// function addition(num1,num2){
+//      let res = num1 + num2
+//      return res
+     
+// } 
+// const res = addition(4, 6)
+// console.log("result: " , res);
+
+//anotherway
+
+// function addition(num1,num2){
+     
+//      return num1 + num2
+     
+// } 
+// const res = addition(4, 6)
+// console.log(res);
+
+function welcome(name){
+    if(name === undefined){
+        console.log("naam likh le bhai");
+        return
+        
+
+    }
+    return `Hi ${name} your most welcome`
+}
+
+console.log(welcome("manish"));
+
+
+
+ 
+
+
